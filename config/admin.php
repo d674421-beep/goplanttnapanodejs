b49aedd056e26f61b73d6160b93be435fb7cb270
jho <d674421@gmail.com>
2026-02-10 12:54:14 +0700

    Initial commit ubah isi config/admin.php

--- config/admin.php
+++ config/admin.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'email' => env('ADMIN_EMAIL'),
+    'email'    => env('ADMIN_EMAIL'),
     'password' => env('ADMIN_PASSWORD'),
-    'name' => env('ADMIN_NAME', 'Admin GoPlant'),
+    'name'     => env('ADMIN_NAME', 'Administrator'),
 ];
